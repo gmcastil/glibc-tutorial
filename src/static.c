3a4a0a110b160a1cba7d36ae883c2982d20d6b46
George Castillo <gmcastil@gmail.com>
2022-06-16 10:49:43 -0600

    Simple example of static variables being initialized when the function is first called.

--- src/static.c
+++ src/static.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint32_t counter() {
-	static uint32_t i = 0;
-	i++;
+uint32_t counter(uint32_t);
+
+uint32_t counter(uint32_t start) {
+
+	static uint32_t call_count = 0;
+	static uint32_t i;
+
+	if ( call_count == 0 ) {
+		i = start;
+	} else {
+		i++;
+	}
+
+	call_count++;
 	return i;
 }
 
@@ -11,13 +22,12 @@ int main()
 {
 	uint32_t current;
 	while (1) {
-		current = counter();
-		if (current == 100) {
-			printf("Count is %d\n", current);
+		current = counter(3);
+		printf("Count is %d\n", current);
+		if ( current == 10 ) {
 			return 0;
 		} else {
 			continue;
 		}
 	};
 }
-
