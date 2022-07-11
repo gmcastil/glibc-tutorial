c4f3c039775a0474941e3b2448d7e102ee75d467
George Castillo <gmcastil@gmail.com>
2022-07-11 06:42:41 -0500

    More work on the minfo example

--- src/virtual/minfo/minfo.c
+++ src/virtual/minfo/minfo.c
@@ -2,21 +2,6 @@
  * Create a program that requests several blocks of heap allocation, stores
  * references to them in an orderly fashion, and displays information about the
  * status of dynamic memory allocation of the process
- *   - Create an array of pointers that will be returned from malloc() calls.
- *   - Create an array of mallinfo2 structs that will be returned from
- *     a sequence of mallinfo2() calls
- *   - For each pointer in the first array, request a block of memory increasing
- *     in size (1KB to 512MB).  After each allocation is
- *     performed, call the mallinfo2() function to obtain information on the
- *     state of dynamic memory allocation, store the result in the second array
- *   - Create a function that pretty prints a `struct mallinfo2` in a single
- *     line to the console
- *   - Using that function, create another function that prints a formatted
- *     table to the console of the state of memory allocation before and after
- *     each of the allocations that are performed.  After this, wait for user
- *     input.
- *   - Upon user pressng a key, free all of the memory that was allocated by the
- *     process and print summary statistics at the end.
 */
 
 #include <stdlib.h>
@@ -24,8 +9,9 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <malloc.h>
+#include <errno.h>
 
-#define NUM 10
+#define NUM 100
 
 int main(int argc, char *argv[])
 {
@@ -41,22 +27,21 @@ int main(int argc, char *argv[])
 	}
 	
 	for (i=0; i<NUM; i++) {
+
+		errno = 0;
 		blocks[i] = malloc(mallsize);
 		if (blocks[i]) {
 			mi = mallinfo();
 			meminfo[i] = &mi;
+			printf("Successful malloc() of %zu bytes\n", mallsize);
 			mallsize = mallsize << 1;
 		} else {
-			printf("Error: Could not malloc() with size 0x%zx\n", mallsize);
+			perror("malloc() returned NULL");
 			return 1;
 		}
 	}
 	for (i=0; i<NUM; i++) {
-		if (blocks[i]) {
-			free(blocks[i]);
-		} else {
-			printf("Error: Could not free memory\n");
-		}
+		free(blocks[i]);
 	}
 	return 0;
 }
