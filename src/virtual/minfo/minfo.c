71e28e2840282e2f6d8f7198485154cd54243d13
George Castillo <gmcastil@gmail.com>
2022-07-11 10:18:39 -0600

    Keeping allocation changes, but unwinding unit change for obvious reasons

--- src/virtual/minfo/minfo.c
+++ src/virtual/minfo/minfo.c
@@ -16,22 +16,22 @@
 void mi_print(struct mallinfo mi)
 {
 	printf("\n");
-	printf("Non-MMAP allocated space (KB):\t\t%8d\n", mi.arena >> 10);
+	printf("Non-MMAP allocated space (bytes):\t\t%8d\n", mi.arena);
 	printf("Ordinary free blocks:\t\t\t%8d\n", mi.ordblks);
 	printf("Number of fastbin free blocks:\t\t%8d\n", mi.smblks);
 	printf("Number of MMAP allocated blocks:\t%8d\n", mi.hblks);
 	printf("Space in MMAP allocated regions:\t%8d\n", mi.hblkhd);
-	printf("Max total allocated space (KB):\t\t%8d\n", mi.usmblks); /* always 0, preserved for backwards compatibility */
-	printf("Space in fastbin freed blocks (KB):\t%8d\n", mi.fsmblks >> 10);
-	printf("Total allocated space (KB):\t\t%8d\n", mi.uordblks >> 10);
-	printf("Total space in freed blocks (KB):\t%8d\n", mi.fordblks >> 10);
-	printf("Releaseable free space (KB):\t\t%8d\n\n", mi.keepcost >> 10);
+	printf("Max total allocated space (bytes):\t\t%8d\n", mi.usmblks); /* always 0, preserved for backwards compatibility */
+	printf("Space in fastbin freed blocks (bytes):\t%8d\n", mi.fsmblks);
+	printf("Total allocated space (bytes):\t\t%8d\n", mi.uordblks);
+	printf("Total space in freed blocks (bytes):\t%8d\n", mi.fordblks);
+	printf("Releaseable free space (bytes):\t\t%8d\n\n", mi.keepcost);
 }
 
 int main(int argc, char *argv[])
 {
 	uint32_t i = 0;
-	uint32_t mall_good_cnt = 0;
+	uint32_t j = 0;
 	size_t mallsize = 1024;
 
 	void *blocks[NUM];
@@ -53,16 +53,15 @@ int main(int argc, char *argv[])
 			mi = mallinfo();
 			meminfo[i] = &mi;
 			mallsize = mallsize << 1;
-			mall_good_cnt++;
 		} else {
 			printf("\t\tFAIL\n");
 			perror("malloc() returned NULL");
 			mi_print(*meminfo[i-1]);
+			for (j=0; j<i; j++) {
+				free(blocks[j]);
+			}
 			break;
 		}
-	}
-	for (i=0; i<mall_good_cnt; i++) {
-		free(blocks[i]);
 	}
 	return 0;
 }
