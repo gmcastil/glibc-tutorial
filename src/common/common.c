b4b078e3c2d5c631394d38796cd93cd93104d283
George Castillo <gmcastil@gmail.com>
2022-07-13 07:05:43 -0600

    Adding a common directory and some commonly used functions
    It occurred to me this morning that I was going to be using a lot of
    common functions over and over again, so I decided to make my own
    library and then reuse it in all of my little example programs and such

---
+++ src/common/common.c
@@ -0,0 +1,9 @@
+void error(char *msg)
+{
+        if (msg) {
+                printf("ERROR: %s\n", msg);
+        } else {
+                printf("ERROR: Unknown\n");
+        }   
+}
+
---
+++ src/common/common.h
@@ -0,0 +1,6 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+void error(char *msg);
+
+#endif /* COMMON_H */
