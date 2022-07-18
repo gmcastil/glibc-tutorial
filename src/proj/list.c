6fa57742baa5a31626ba9ba3d7df68b999398511
George Castillo <gmcastil@gmail.com>
2022-07-18 07:07:47 -0600

    Manually recursive list of 3 elements

--- src/proj/list.c
+++ src/proj/list.c
@@ -16,19 +16,45 @@ struct list *init_list(uint32_t length)
 {
 	uint32_t i = 0;
 	struct node *nptr = NULL;
+	struct node *next_nptr = NULL;
 
-	struct list *lptr = malloc(sizeof(struct list));;
+	struct list *lptr = malloc(sizeof(struct list));
 	if (lptr) {
-		while (i < length) {
+		/* create and populate n = 0 */
+		nptr = (struct node *) malloc(sizeof(struct node));
+		if (nptr) {
+			/* indicate the start of the list */
+			lptr->head = nptr;
+			lptr->tail = nptr;
+			lptr->length = 1;
+
+			(uint32_t *) nptr->element = &i;
+			/* create and populate n = 1 */
+			next_nptr = (struct node *) malloc(sizeof(struct node));
+			if (next_nptr) {
+				i++;
+				nptr->next = next_nptr;
+				(uint32_t *) next_nptr->element = &i;
+			} else {
+				printf("couldnt dereference NULL pointer at n = 1\n");
+			}
+			lptr->tail = next_nptr;
+			lptr->length = 2;
+			
+			/* create and populate n = 2 */
 			nptr = (struct node *) malloc(sizeof(struct node));
 			if (nptr) {
-				lptr->head = nptr;
-				lptr->tail = nptr;
-				lptr->length = 1;
+				i++;
+				next_nptr->next = nptr;
+				(uint32_t *) nptr->element = &i;
 			} else {
-				printf("couldnt dereference NULL pointer\n");
+				printf("couldnt dereference NULL pointer at n = 2\n");
 			}
-			i++;
+			lptr->tail = nptr;
+			lptr->length = 3;
+			
+		} else {
+			printf("couldnt dereference NULL pointer at n = 0 stage of construction\n");
 		}
 	}
 
