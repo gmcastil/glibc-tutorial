a4c4be62f01ab0631be1e9b8e30600e1618a8bc1
George Castillo <gmcastil@gmail.com>
2022-07-17 19:29:21 -0500

    Successfully created a list of length 1.  Should also refactor things to use a single Makefile for each chapter in the GNU C library

--- src/proj/list.c
+++ src/proj/list.c
@@ -5,34 +5,32 @@
  * - Support insertion, deletion
 */
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
 #include "list.h"
 
-struct node {
-        void *element;
-        void *next;
-        void *prev;
-};
-
-struct list init_list(uint32_t length)
+struct list *init_list(uint32_t length)
 {
 	uint32_t i = 0;
-	struct node *pnode = NULL;
+	struct node *nptr = NULL;
 
-	struct list l;
-
-	if (length == 0) {
-		l.head = NULL;
-		l.tail = NULL;
-		l.length = length;
-	} else if (length <= UINT32_MAX) {
-		for (i=0; i<length; i++) {
-			/* maybe some sort of recursvie thing where I call a
-			 * function to create a node with a pointer to the
-			 * previous one and return NULL at the end */
+	struct list *lptr = malloc(sizeof(struct list));;
+	if (lptr) {
+		while (i < length) {
+			nptr = (struct node *) malloc(sizeof(struct node));
+			if (nptr) {
+				lptr->head = nptr;
+				lptr->tail = nptr;
+				lptr->length = 1;
+			} else {
+				printf("couldnt dereference NULL pointer\n");
+			}
+			i++;
 		}
 	}
-	return l;
+
+	return lptr;
 }
--- src/proj/list.h
+++ src/proj/list.h
@@ -1,12 +1,17 @@
 #ifndef LIST_H
 #define LIST_H
 
+struct node {
+	void *element;
+	void *next;
+};
+
 struct list {
-        void *head;
-        void *tail;
+        struct node *head;
+        struct node *tail;
         uint32_t length;
 };
 
-struct list init_list(uint32_t length);
+struct list *init_list(uint32_t length);
 
 #endif /* LIST_H */
